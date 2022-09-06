8eb861130e8a887b96f4750b3ee2aee4603e6131
Eric Peterson <ericpeterson91@live.ca>
2022-09-05 18:05:33 -0600

    game starting to work

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ console.log(squares)
 
 squares.forEach((square, i) => {
     square.addEventListener('click', () => {
-        alert('You clicked square ' + i)
+        if (squares[i + 7].classList.contains('taken')) {
+            if (currentPlayer == 1) {
+                squares[i].classList.add('taken')
+                squares[i].classList.add('player-one')
+                currentPlayer = 2
+                displayCurrentPlayer.innerText = currentPlayer
+            } else if (currentPlayer == 2) {
+                squares[i].classList.add('taken')
+                squares[i].classList.add('player-two')
+                currentPlayer = 1
+                displayCurrentPlayer.innerText = currentPlayer
+            }
+        }  
+
     })
-})
+        
+    })
